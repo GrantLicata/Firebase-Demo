68d6551e5b390178f9165dbf37f106d66b061552
GrantLicata <grant.licata@gmail.com>
2023-03-18 13:03:40 -0700

    Firebase regstration and installation

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Functions imported from the SDKs
+import { initializeApp } from "firebase/app";
+
+// The app's Firebase configuration
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyCEjkn3MbIduZ4v3W7bLClJhWmv4UMRKUw",
+    authDomain: "fir-73677.firebaseapp.com",
+    projectId: "fir-73677",
+    storageBucket: "fir-73677.appspot.com",
+    messagingSenderId: "448275358291",
+    appId: "1:448275358291:web:0c65b7794c9fbee6c81ca6"
+})
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
